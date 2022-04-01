7295516f3404720b695c953813d4eded553dd8de
Daniel Ulises <daniel.cau@pm.me>
2022-04-01 19:56:18 +0200

    Removing the backend folder from the repository

--- backend/api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Process(models.Model):
-    uid = models.IntegerField()
-    user = models.CharField(max_length=200)
-    pid = models.IntegerField()
-    cmd = models.TextField(null=False, blank=False)
--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("cache", views.cachePs, name="cache"),
-    path("get", views.getPs, name="get")
-]
