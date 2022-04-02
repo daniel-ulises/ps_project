93cb3fc4d3602a6ba05ec9086b391217933288a5
Daniel Ulises <daniel.cau@pm.me>
2022-04-02 20:21:04 +0200

    Rewrite the views to display extra data in the JSON response. Added commentaries for the code

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+# Following routes are:
+# cache: Execute the bash script to fill the database with running processes
+# get/ps: Get the saved processes
+# get/users: Get users with a process attatched to them
+# get/<int:uid>: Get running processes attatched to a user, by their UID
 urlpatterns = [
     path("cache", views.cachePs, name="cache"),
-    path("get", views.getPs, name="get"),
+    path("get/ps", views.getPs, name="get"),
     path("get/users", views.getUsers, name="get_users"),
     path("get/<int:uid>", views.getById, name="get_by_id")
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -1,7 +1,6 @@
-from django.core import serializers
+from django.core.serializers import serialize
 from django.conf import settings
-from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
 from .models import Process, Users
 import subprocess, os, json
 
@@ -15,22 +14,25 @@ def cachePs(request):
     msg = {
         "body": "PS output cached successfully"
     }
-    return JsonResponse(msg)
+    return JsonResponse(msg, safe=False)
 
 # Here the saved data is serialized, in order to display it as JSON to the front end. 
 # This will make the data readable for the front end, so it can be displayed in the browser.
 def getPs(request):
-    ps = serializers.serialize("json", Process.objects.all())
-    output = [d['fields'] for d in json.loads(ps)]
-    return JsonResponse(output, safe=False)
+    ps = json.loads(serialize("json", Process.objects.all()))
+    return JsonResponse(ps, safe=False)
 
+# This will get the running processes for a specific user that will be selected
+# on the front end. 
 def getById(request, uid):
-    psByUid = serializers.serialize("json", Process.objects.filter(uid=uid))
-    output = [d['fields'] for d in json.loads(psByUid)]
-    return JsonResponse(output, safe=False)
+    psByUid = json.loads(serialize("json", Process.objects.filter(uid=uid)))
+    return JsonResponse(psByUid, safe=False)
 
+# This query will return a list of users to display them in the 'select' element
+# on the front end. In order to avoid displaying users from the system that
+# are not running a process, the query filters the users to show only
+# the ones that also have a running process attatched to them.
 def getUsers(request):
     subquery = Process.objects.values_list("uid", flat=True).distinct()
-    users = serializers.serialize("json", Users.objects.filter(uid__in=subquery))
-    output = [d['fields'] for d in json.loads(users)]
-    return JsonResponse(output, safe=False)
+    users = json.loads(serialize("json", Users.objects.filter(uid__in=subquery)))
+    return JsonResponse(users, safe=False)
